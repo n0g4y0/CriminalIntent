19a9990b77ff772de459a21dff00e35bdfb19a63
n0g4y0 <n0g4y0@gmail.com>
2019-08-17 18:45:51 -0400

    primer commit, proyecto criminalIntent, se hara un ejemplo del manejo de intents, se va configurando inicialmente el Fragment inicial.

---
+++ app/src/main/java/io/github/n0g4y0/criminalintent/CrimeFragment.kt
@@ -0,0 +1,91 @@
+package io.github.n0g4y0.criminalintent
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import io.github.n0g4y0.criminalintent.models.Crime
+
+class CrimeFragment : Fragment() {
+    private lateinit var crime :Crime
+    // inicializando los WIDGETS:
+    private lateinit var titleField: EditText
+
+    // ahora el boton:
+    private lateinit var dateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    /*
+    * con esta funcion, creamos, o inflamos los GRAFICOS en el ACTIVITY PRINCIPAL:
+    * NOTA: esta funcion, es el lugar donde se conectan los WIDGETS:
+    * */
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       val view =  inflater.inflate(R.layout.fragment_crime,container,false)
+
+        // conectando el WIDGET edittext (de titulo TITLE) a la variable del mismo tipo, inicializado a un inicio.
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        // conectando al boton:
+        dateButton = view.findViewById(R.id.crime_date) as Button
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+        return view
+    }
+
+    /*
+    * funcion para INICIALIZAR y agregar un ESCUCHA (LISTENER) al WIDGET:
+    * */
+
+    override fun onStart() {
+        super.onStart()
+
+        /*
+        * una de las razones para implementar esto: es para evitar que se pierda el dato ingresado (edittext y checkbox)
+        * al momento de girar la pantalla.
+        * */
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                // This space intentionally left blank
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // this One too
+            }
+
+        }
+        titleField.addTextChangedListener(titleWatcher)
+
+    }
+
+}
---
+++ app/src/main/java/io/github/n0g4y0/criminalintent/models/Crime.kt
@@ -0,0 +1,12 @@
+package io.github.n0g4y0.criminalintent.models
+
+import java.util.*
+
+/*
+* Clase para manejar los crimenes, representa el MODELO del proyecto.
+* */
+
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false)
