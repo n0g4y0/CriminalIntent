9affd9032c25b17177d310cde9f38ee8ec79e1ce
n0g4y0 <n0g4y0@gmail.com>
2019-08-18 17:51:51 -0400

    pasos para implementar el RecyclerView, desde agregar la extension para manejarlo (GRADLE), hasta la funcion OnCreateView, tambien se agrega el Objeto LayoutManager, falta que muestre datos

--- app/src/main/java/io/github/n0g4y0/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/CrimeListFragment.kt
@@ -2,12 +2,19 @@ package io.github.n0g4y0.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+
+    private lateinit var crimeRecyclerView : RecyclerView
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -19,6 +26,27 @@ class CrimeListFragment : Fragment() {
         // inicialmente, al llamar al fragment, solo mostrara esto en consola:
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
     }
+
+    /*
+    * funcion que visualiza o INFLATE la vista del fragment (en este caso, el de FRAGMENT_CRIME_LIST):
+    *
+    * */
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime_list,container,false)
+
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
+
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
+    }
+
+
     companion object {
         fun newInstance(): CrimeListFragment {
             return CrimeListFragment()
