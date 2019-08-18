1e03da63de856d493f6b93f4d5fa00b2e279fa4c
n0g4y0 <n0g4y0@gmail.com>
2019-08-18 12:24:02 -0400

    finalmente, se muestra el fragment, en el MainActivity

--- app/src/main/java/io/github/n0g4y0/criminalintent/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/MainActivity.kt
@@ -8,5 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // empezando a usar el FRAGMENT MANAGER:
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null){
+            // asignamos el FRAGMENT que deseamos manejar, a una variable, para luego manipularlo.
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                //desde aqui, especificamos que queremos hacer con el FRAGMENT en cuestion:
+                .add(R.id.fragment_container,fragment)
+                .commit()
+        }
+
     }
 }
