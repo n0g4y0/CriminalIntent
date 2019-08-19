862f7a51fddd861169c99f9d60a8bead47e04a79
n0g4y0 <n0g4y0@gmail.com>
2019-08-19 10:01:20 -0400

    se agrega un LAYOUT para mostrar cada ITEM del recyclerView, finalmente funciona

--- app/src/main/java/io/github/n0g4y0/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/CrimeListFragment.kt
@@ -5,16 +5,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.github.n0g4y0.criminalintent.models.Crime
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView : RecyclerView
+    //para conectar con el adaptador:
+    private var adapter: CrimeAdapter? = null
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
@@ -43,7 +47,16 @@ class CrimeListFragment : Fragment() {
 
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
+        // funcion para conectar el RecyclerView con el ADAPTER, para mostrar los ITEMS
+        updateUI()
+
         return view
+    }
+
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
     }
 
 
@@ -52,4 +65,38 @@ class CrimeListFragment : Fragment() {
             return CrimeListFragment()
         }
     }
+
+    private inner class CrimeHolder(view: View)
+        : RecyclerView.ViewHolder(view){
+
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>)
+        :RecyclerView.Adapter<CrimeHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+                : CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime,parent,false)
+            return CrimeHolder(view)
+
+        }
+
+        override fun getItemCount() = crimes.size
+
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+
+        }
+
+
+    }
+
 }
