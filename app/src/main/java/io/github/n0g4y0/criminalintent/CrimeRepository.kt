c1709248e9041e0b728533be896521d50aea90ea
n0g4y0 <n0g4y0@gmail.com>
2019-08-24 18:16:39 -0400

    configurando propiedades de la clase repositorio, haciendo la conexion con la BDCrime

--- app/src/main/java/io/github/n0g4y0/criminalintent/CrimeRepository.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/CrimeRepository.kt
@@ -1,9 +1,22 @@
 package io.github.n0g4y0.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import io.github.n0g4y0.criminalintent.database.CrimeDao
+import io.github.n0g4y0.criminalintent.database.CrimeDatabase
 import java.lang.IllegalStateException
 
+private const val DATABASE_NAME = "crime-database"
+
 class CrimeRepository private constructor(context: Context){
+
+    private val database : CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+        ).build()
+
+    private val CrimeDao = database.crimeDao()
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
