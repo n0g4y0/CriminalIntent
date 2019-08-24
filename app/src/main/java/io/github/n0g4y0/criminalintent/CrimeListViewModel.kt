e9cdf69d7a17f17593c9bb7062e62bf524061eed
n0g4y0 <n0g4y0@gmail.com>
2019-08-24 19:11:47 -0400

    se copiaron archivos de BD, mediante el FILE EXPLORER de android, y existe actualmente un error de THREAD principal, ya que el hilo principal,esta siendo usado por la BD.

--- app/src/main/java/io/github/n0g4y0/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/CrimeListViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.ViewModel
 import io.github.n0g4y0.criminalintent.models.Crime
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
 
-    // por ahora, la inicializamos con datos ficticios:
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        }
-    }
+    /*
+    *
+    * las siguientes lineas, extraen los datos, desde unos archivos que acabamos de copiar,
+    * que son bases de datos pre cargados.
+    *
+    * */
+
+     private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
+
 }
