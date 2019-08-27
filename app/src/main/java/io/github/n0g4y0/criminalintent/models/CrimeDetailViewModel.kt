6c2f1879b3f4dff567e354e71a64bada5015b72d
n0g4y0 <n0g4y0@gmail.com>
2019-08-27 12:26:55 -0400

    añadiendo la capacidad de CrimeDetails, de guardar los cambios que realize el usuario en la UI, esto es muy facil de integrar, gracias al CrimeRepository

--- app/src/main/java/io/github/n0g4y0/criminalintent/models/CrimeDetailViewModel.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/models/CrimeDetailViewModel.kt
@@ -22,4 +22,7 @@ class CrimeDetailViewModel : ViewModel(){
     fun loadCrime(crimeId: UUID){
         crimeIdLiveData.value = crimeId
     }
+    fun saveCrime(crime:Crime){
+        crimeRepository.updateCrime(crime)
+    }
 }
