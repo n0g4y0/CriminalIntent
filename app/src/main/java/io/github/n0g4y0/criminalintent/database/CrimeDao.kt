aa91b2825e02d50fb7b070b78e6c2494af0213cb
n0g4y0 <n0g4y0@gmail.com>
2019-08-27 12:09:04 -0400

    creo nuevas funciones para la actualizar e insertar datos en la BD,falta implementar el EXECUTOR

--- app/src/main/java/io/github/n0g4y0/criminalintent/database/CrimeDao.kt
+++ app/src/main/java/io/github/n0g4y0/criminalintent/database/CrimeDao.kt
@@ -2,7 +2,9 @@ package io.github.n0g4y0.criminalintent.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import io.github.n0g4y0.criminalintent.models.Crime
 import java.util.*
 
@@ -14,4 +16,10 @@ interface CrimeDao {
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     fun getCrime(id: UUID): LiveData<Crime?>
+
+    @Update
+    fun updateCrime(crime:Crime)
+
+    @Insert
+    fun addCrime(crime:Crime)
 }
